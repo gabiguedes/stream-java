55b3570182f9d5b6c854823656d05714deb9221f
gabiguedes <gabriela_rayssa@hotmail.com>
2022-05-10 13:03:48 -0300

    ajuste reduce

--- src/reduce/Reduce.java
+++ src/reduce/Reduce.java
@@ -12,6 +12,7 @@ public class Reduce {
 	public int somaReduce() {
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 		int result = numbers.stream().reduce(0, (subtotal, element) -> subtotal + element);
+		//assertThat(result).isEqualTo(21);
 		return result;
 	}
 	
@@ -22,6 +23,16 @@ public class Reduce {
 		Integer reduced = inteiros.stream().reduce(numeroRecebido, (a, b) -> a + b);
 		
 		return reduced;
+	}
+	
+	public String concatStrings() {
+		List<String> letters = Arrays.asList("a", "b", "c", "d", "e");
+		String result = letters
+				  .stream()
+				  .reduce("", (partialString, element) -> partialString + element);
+				  //assertThat(result).isEqualTo("abcde");
+		
+		return result;
 	}
  
 }
